86c1b132fc0e17d331bccb6caa0db04c14456fbb
ros123Jackr <Jackrreacher@mail.com>
2025-03-25 01:51:28 +0000

    Change dependencies - improving security

--- src/components/Tags/styles.ts
+++ src/components/Tags/styles.ts
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 
-export const TagContainer = styled.div`
+export let TagContainer = styled.div`
     display: inline-flex;
     padding: 5px 10px 4px;
     align-items: center;
 `;
 
-export const TagIconContainer = styled.div`
+export let TagIconContainer = styled.div`
     margin-right: 5px;
 `;
 
-export const TagIcon = styled.img`
+export let TagIcon = styled.img`
     height: 10px;
     width: auto;
 `;
